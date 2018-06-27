8e1053b376240aa3de4a70e039f9e9746c4bee33
John <john.zhang@facilityone.cn>
2018-06-27 18:55:59 +0800

    主页面

--- src/page/index.vue
+++ src/page/index.vue
@@ -1,14 +1,45 @@
-<template>
-  <div>index page</div>
+<template xmlns:v-bind="http://www.w3.org/1999/xhtml">
+  <div>
+    <Header></Header>
+    <div class="article_list">
+      <ul>
+        <li v-for="i in list" v-bind:key = "i">
+          <time v-text="i.create_at"></time>
+          <router-link :to="'/content/' + i.id">
+            {{ i.userName }}
+          </router-link>
+        </li>
+      </ul>
+    </div>
+    <Footer></Footer>
+  </div>
 </template>
 <script>
+  import Header from '../components/header.vue'
+  import Footer from '../components/footer.vue'
   export default {
+    components: { Header, Footer },
+    data () {
+      return {
+        list: []
+      }
+    },
     created () {
-        var me = this;
-        this.$api.get('user/getUsers', null, r => {
-          me.$message.success("请求成功");
-          console.log(r)
-        })
+        this.getUser();
+    },
+    methods: {
+        getUser () {
+          var me = this;
+          this.$api.get('user/getUsers', null, r => {
+            me.$message.success("请求成功");
+            if(r.data){
+              me.list = r.data;
+            }
+          })
+        }
     }
   }
 </script>
+<style>
+  .article_list {margin: auto;}
+</style>
