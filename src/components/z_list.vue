ea757423a5399eaaaff46bcc32a3ed5bc6fc8b60
ZNG-GITHUB <1159427683@qq.com>
2018-10-18 01:41:53 +0800

    '权限管理界面'

--- src/components/z_list.vue
+++ src/components/z_list.vue
@@ -3,23 +3,6 @@
     <ul>
       <li class="card-li" :class="{'li-select':(li.id == activeItem)}" @click="select(li.id)" v-for="li in list" v-bind:key="li.id">{{li.name}}</li>
     </ul>
-    <!--<span style="cursor:auto">auto</span><br>
-    <span style="cursor:crosshair">crosshair</span><br>
-    <span style="cursor:default">default</span><br>
-    <span style="cursor:e-resize">e-resize</span><br>
-    <span style="cursor:help">help</span><br>
-    <span style="cursor:move">move</span><br>
-    <span style="cursor:n-resize">n-resize</span><br>
-    <span style="cursor:ne-resize">ne-resize</span><br>
-    <span style="cursor:nw-resize">nw-resize</span><br>
-    <span style="cursor:pointer">pointer</span><br>
-    <span style="cursor:progress">progress</span><br>
-    <span style="cursor:s-resize">s-resize</span><br>
-    <span style="cursor:se-resize">se-resize</span><br>
-    <span style="cursor:sw-resize">sw-resize</span><br>
-    <span style="cursor:text">text</span><br>
-    <span style="cursor:w-resize">w-resize</span><br>
-    <span style="cursor:wait">wait</span><br>-->
   </div>
 </template>
 <script>
@@ -28,9 +11,7 @@
       list:{
         type:Array
       },
-      activeItem:{
-        type:String
-      }
+      activeItem:{}
     },
     data () {
       return {
@@ -52,10 +33,11 @@
     list-style: none;
     cursor:pointer;
     padding: 10px 20px;
+    font-size: 14px;
   }
   .card-li.li-select{
     color: cadetblue;
-    background-color: whitesmoke;
+    background-color: #E6E6E6;
   }
   .card-li:hover{
     background-color: whitesmoke;
